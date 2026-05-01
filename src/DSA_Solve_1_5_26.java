1fe83641f9489342f9f3c6e7e2fb6293a0e35021
Ramen Mondal <rmofficial01x@gmail.com>
2026-05-01 22:47:35 +0530

    My first code simple but I attend the journey 1-5-26

---
+++ src/DSA_Solve_1_5_26.java
@@ -0,0 +1,11 @@
+public class DSA_Solve_1_5_26 {
+    public static void main(String[] args) {
+        int sumAns = sum(10,18);
+        System.out.println("The Sum ans: " + sumAns);
+    }
+
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+}
